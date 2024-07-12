c9d826feeb8e6c2e75f221ad75f400aeec5f94be
Your Name <you@example.com>
2024-07-12 08:45:11 +0800

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,28 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            I excel in color grading, audio editing, and creating seamless transitions that elevate the viewer's experience. Collaboration is at the heart of my work, and I thrive in both solo and team environments. Whether you're looking to create a captivating commercial or an emotionally resonant short film, I'm here to bring your vision to life. Explore my portfolio and let's create something amazing together!
+
+
+
+
+
+
+
+          </p>
+          <p className="hero--section-description">
+
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Mark</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Web</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            As a dedicated video editor, I transform raw footage into compelling visual stories. My journey in video editing began with a passion for storytelling and a keen eye for detail. Over the years, I’ve honed my skills in various editing software, including Adobe Premiere Pro, Final Cut Pro, and DaVinci Resolve. My portfolio showcases a diverse range of projects, from corporate videos and commercials to music videos and short films. Each project is a testament to my ability to blend creativity with technical precision. 
+            <br />
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
