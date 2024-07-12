64fa4f05bac1541b7d77cbc7d994d095323a6733
arashikage95 <121141727+arashikage95@users.noreply.github.com>
2024-07-12 08:59:13 +0800

    Update HeroSection.jsx

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -5,9 +5,9 @@ export default function HeroSection() {
         <div className="hero--section--content">
           <p className="section--title">Hey, I'm Mark</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Web</span>{" "}
+            <span className="hero--section-title--color">Video</span>{" "}
             <br />
-            Developer
+            Editor
           </h1>
           <p className="hero--section-description">
             As a dedicated video editor, I transform raw footage into compelling visual stories. My journey in video editing began with a passion for storytelling and a keen eye for detail. Over the years, I’ve honed my skills in various editing software, including Adobe Premiere Pro, Final Cut Pro, and DaVinci Resolve. My portfolio showcases a diverse range of projects, from corporate videos and commercials to music videos and short films. Each project is a testament to my ability to blend creativity with technical precision. 
